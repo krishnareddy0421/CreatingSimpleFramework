852bb82a764fb858c62906b9a5ad6ce251ad43b9
vamsi krishna reddy kamjula <krishnareddy0421@gmail.com>
2017-10-19 11:12:44 -0400

    Custom Cell Created

---
+++ CreatingFramework/CustomTableCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableCell.swift
+//  CreatingFramework
+//
+//  Created by vamsi krishna reddy kamjula on 10/19/17.
+//  Copyright © 2017 kvkr. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableCell: UITableViewCell {
+
+    @IBOutlet public weak var stateName: UILabel!
+    @IBOutlet public weak var stateArea: UILabel!
+    @IBOutlet public weak var stateAbbr: UILabel!
+    @IBOutlet public weak var stateLargestCity: UILabel!
+    @IBOutlet public weak var country: UILabel!
+    @IBOutlet public weak var stateCapital: UILabel!
+}
